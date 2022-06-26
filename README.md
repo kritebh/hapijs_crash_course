32c65dee67695059565cd5898bff606c8c280440
kritebh <kritebhlaganbibhakar@gmail.com>
2022-06-26 16:23:25 +0530

    auth done

---
+++ README.md
@@ -0,0 +1,5 @@
+## HAPI JS Crash Course
+
+### Hapi JS Crash Course with basic authentication 
+
+- Tutorial Playlist - [YouTube](https://www.youtube.com/playlist?list=PLkqiWyX-_LotaQ9AuppIAXl0xyV-P5Ms-)
--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const Hapi = require("@hapi/hapi")
+const path = require('path')
+const Boom = require('@hapi/boom')
 
 const server = Hapi.server({
     port: 8000,
     host: 'localhost'
 });
 
+
+const validate = async (req, username, password, h) => {
+    if (username === "admin" && password === "admin") {
+        return {
+            isValid: true,
+            credentials: {
+                id: 1,
+                name: "admin"
+            }
+        }
+    }
+    else {
+        return { isValid: false, credentials: null }
+    }
+}
+
 //Init Server
 const init = async () => {
-    await server.start();
+
     await server.register([{
         plugin: require("hapi-geo-locate"),
         options: {
@@ -16,8 +34,70 @@ const init = async () => {
     },
     {
         plugin: require("@hapi/inert")
+    },
+    {
+        plugin: require("@hapi/vision")
+    },
+    {
+        plugin: require("@hapi/basic")
     }
+    ]);
+
+    server.views({
+        engines: {
+            hbs: require('handlebars')
+        },
+        path: path.join(__dirname, 'views')
+    })
+
+    server.auth.strategy('simple', "basic", { validate })
+
+    server.route([{
+        method: "GET",
+        path: "/loginbasic",
+        options: {
+            auth: 'simple'
+        },
+        handler: (req, h) => {
+            return "Welcome to Dashboard"
+        }
+    },
+    {
+        method: "GET",
+        path: "/",
+        handler: (req, h) => {
+            const data = {
+                name: "kritebh"
+            }
+            return h.view("index", data)
+        }
+    },
+    {
+        method: "GET",
+        path: "/about",
+        handler: (req, h) => {
+            return ("<h1>About Us</h1>")
+        }
+    },
+    {
+        method: "POST",
+        path: "/login",
+        handler: (req, h) => {
+            console.log(req.payload.username)
+            console.log(req.payload.password)
+            return "Login Successful"
+        }
+    },
+    {
+            method: "GET",
+            path: "/logoutbasic",
+            handler: (req, h) => {
+                return Boom.unauthorized("Logout Successfull")
+            }
+        }
     ])
+
+    await server.start();
     console.log('Server running on %s', server.info.uri);
 };
 
@@ -28,21 +108,16 @@ process.on("unhandledRejection", (err) => {
 
 init();
 
-server.route({
-    method: "GET",
-    path: "/",
-    handler: (req, h) => {
-        return h.file("./public/index.html")
-    }
-})
+// server.route(
+//     {
+//         method: "GET",
+//         path: "/",
+//         handler: (req, h) => {
+//             return h.file("./public/index.html")
+//         }
+//     },
+// )
 
-server.route({
-    method: "GET",
-    path: "/about",
-    handler: (req, h) => {
-        return ("<h1>About Us</h1>")
-    }
-})
 server.route({
     method: "GET",
     path: "/locate",
